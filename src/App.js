e572b089ab0d0bb546b10c9b6ead4941117c5f4f
Sagar <sagarpatkar17@gmail.com>
2021-02-02 20:22:12 +0530

    LoginForm, authenticate user

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import { ChatEngine } from "react-chat-engine";
 import "./app.css";
 import ChatFeed from "./components/ChatFeed.jsx";
+import LoginForm from "./components/LoginForm.jsx";
 import dotenv from "dotenv";
 dotenv.config();
 
 const App = () => {
-  const projectId = `${process.env.PROJECT_ID}`;
-  console.log(projectId);
+  if (!localStorage.getItem("username")) {
+    return <LoginForm />;
+  }
   return (
     <ChatEngine
       height="100vh"
       projectID="91971c62-f146-4b94-a915-d626ea3e5df1"
-      userName="sagar@example.com"
-      userSecret="12345"
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
     />
   );
