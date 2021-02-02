b1982ad75e7e576e4cd1fbe4fcc94de41f83b307
Sagar <sagarpatkar17@gmail.com>
2021-02-02 12:46:51 +0530

    Chat Application - Create React file - ChatFeed.jsx file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { ChatEngine } from "react-chat-engine";
+import "./app.css";
+import ChatFeed from "./components/ChatFeed.jsx";
+import dotenv from "dotenv";
+dotenv.config();
 
-function App() {
+const App = () => {
+  const projectId = `${process.env.PROJECT_ID}`;
+  console.log(projectId);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatEngine
+      height="100vh"
+      projectID="91971c62-f146-4b94-a915-d626ea3e5df1"
+      userName="sagar@example.com"
+      userSecret="12345"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    />
   );
-}
+};
 
 export default App;
